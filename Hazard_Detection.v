053f41ef040564ceada1c538da4b56b8344593f7
rasta tadayon <rasta.tadayon1378@gmail.com>
2021-05-11 14:03:26 +0430

    Complete hazard

--- Hazard_Detection.v
+++ Hazard_Detection.v
@@ -2,7 +2,8 @@
 
 module Hazard_Detection (
     two_src,
-    r_n,
+    r_n, //src1
+    r_d, //src2
     mem_dest,
     mem_wb_en,
     exe_dest,
@@ -11,9 +12,15 @@ module Hazard_Detection (
     hazard
 );
 
-input two_src, mem_wb_en, exe_wb_en;
-input[`REGISTER_FILE_ADDRESS_LEN - 1 : 0] r_n, mem_dest, exe_dest;
+    input two_src, mem_wb_en, exe_wb_en;
+    input[`REGISTER_FILE_ADDRESS_LEN - 1 : 0] r_n, r_d, mem_dest, exe_dest;
 
-output hazard;
+    output hazard;
+    
+    assign hazard = ((r_n == exe_dest) && (exe_wb_en == 1'b1)) ? 1'b1
+            : ((r_n == mem_dest) && (mem_wb_en == 1'b1)) ? 1'b1
+            : ((r_d == exe_dest) && (exe_wb_en == 1'b1) && (two_src == 1'b1)) ? 1'b1
+            : ((r_d == mem_dest) && (mem_wb_en == 1'b1) && (two_src == 1'b1)) ? 1'b1
+            : 1'b0;
 
 endmodule

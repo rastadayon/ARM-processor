d1d64ab33e0a7d2c00dee8c6e808361935a3c4ae
zahramo <moosavizahra67@yahoo.com>
2021-06-24 22:20:40 +0430

    Fix cache timing bug

--- Cache_Controller.v
+++ Cache_Controller.v
@@ -45,21 +45,25 @@ reg hit;
 reg[`ADDRESS_LEN-1:0] address_reg;
 
 assign sram_address = address;
-assign cache_address = address_reg[18:2];
+assign cache_address = address[18:2];
 
 always @(posedge clk , posedge rst)begin
     if(rst) ps <= `IDLE_STATE;
     else ps <= ns;
     end
 
-// assign ready = ps == `IDLE_STATE ? `ONE : `ZERO;
+assign ready = ns == `IDLE_STATE ? `ONE : `ZERO;
 
-assign ready = (ps == `IDLE_STATE && (read_en || write_en)) ? `ZERO :
-                (ps == `END_STATE) ? `ONE:
-                (ps == `IDLE_STATE) ? `ONE: ready;
+assign offset = sram_address[2];
 
-assign read_data = (ps == `END_STATE && read_en && cache_hit) ? cache_read_data : 
-                    (ns == `CACHE_WRITE_STATE) ? sram_read_data :
+// assign ready = (ps == `IDLE_STATE && (read_en && cache_hit)) ? `ONE :
+//                 (ps == `IDLE_STATE && (read_en || write_en)) ? `ZERO :
+//                 // (ps == `END_STATE) ? `ONE:
+//                 (ns == `IDLE_STATE) ? `ONE:
+//                 (ps == `IDLE_STATE) ? `ONE: ready;
+
+assign read_data = (ps == `IDLE_STATE && read_en && cache_hit) ? cache_read_data : 
+                   (ps == `MEM_READ_STATE && sram_ready) ? (offset ? sram_read_data[63:32] : sram_read_data[31:0]) :
                     read_data;
                     // (ps == `CACHE_WRITE_STATE) ? read_data :
                     // `CACHE_DATA_LEN'bz;
@@ -67,41 +71,43 @@ assign read_data = (ps == `END_STATE && read_en && cache_hit) ? cache_read_data 
 assign sram_write_data = write_data;
 
 assign sram_write_en = (ps == `IDLE_STATE) && write_en ? `ONE : `ZERO;
+// assign sram_write_en = (ps == `MEM_WRITE_STATE) ? `ONE : `ZERO;
 
 assign cache_read_en = (ps == `IDLE_STATE) && read_en ? `ONE : `ZERO;
 
-assign sram_read_en = (ns == `MEM_READ_STATE) ? `ONE: `ZERO;
+assign sram_read_en = (ps == `MEM_READ_STATE) ? `ONE: `ZERO;
+// assign sram_read_en = (ps == `IDLE_STATE) && read_en && ~cache_hit ? `ONE: `ZERO;
 
-assign cache_write_data = (ns == `CACHE_WRITE_STATE) ? sram_read_data: `CACHE_BLOCK_DATA_LEN'bz;
+assign cache_write_data = sram_read_data;
 
-assign cache_write_en = (ns == `CACHE_WRITE_STATE) ? `ONE: `ZERO;
+assign cache_write_en = (ps == `MEM_READ_STATE && sram_ready == `ONE) ? `ONE: `ZERO;
 
-assign cache_invalidation = (ns == `CACHE_INVALID_STATE) ? `ONE: `ZERO;
+assign cache_invalidation = (ps == `MEM_WRITE_STATE) ? `ONE: `ZERO;
 
 always @(ps, write_en, read_en, cache_hit, sram_ready) begin
     case(ps)
         `IDLE_STATE: begin  
-            ns <= read_en && cache_hit ? `END_STATE :
-                  read_en ? `MEM_READ_STATE:
+            ns <= read_en && cache_hit ? `IDLE_STATE :
+                  read_en && ~cache_hit? `MEM_READ_STATE:
                   write_en ? `MEM_WRITE_STATE :
                   `IDLE_STATE;
             address_reg <= address;
         end
         `MEM_READ_STATE: begin
-            ns <= sram_ready ? `CACHE_WRITE_STATE : `MEM_READ_STATE;
+            ns <= sram_ready ? `IDLE_STATE : `MEM_READ_STATE;
         end
-        `CACHE_WRITE_STATE: begin
-            ns <= `END_STATE;
-        end
+        // `CACHE_WRITE_STATE: begin
+        //     ns <= `IDLE_STATE;
+        // end
         `MEM_WRITE_STATE: begin
             ns <= sram_ready ? `CACHE_INVALID_STATE : `MEM_WRITE_STATE;
         end
         `CACHE_INVALID_STATE: begin
-            ns <= `END_STATE;
-        end
-        `END_STATE: begin
             ns <= `IDLE_STATE;
         end
+        // `END_STATE: begin
+        //     ns <= `IDLE_STATE;
+        // end
         default:;  
     endcase
 end
